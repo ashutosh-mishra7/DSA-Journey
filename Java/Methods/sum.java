76251a8430b9cd88ad8a7cbbf880a263c9c2cc75
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-04-12 19:29:48 +0530

    update

---
+++ Java/Methods/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    static void printSum(int x, int y){
+        int ans = x + y;
+        System.out.println("Sum = " + ans);
+    }    
+    public static void main() {
+        printSum(5,10);
+    }
+
+}
