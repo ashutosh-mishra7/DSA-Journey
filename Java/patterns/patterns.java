35e16eb3f391bd27567b0eb32aaa5323d5519032
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-04-12 17:30:34 +0530

    update

--- Java/patterns/patterns.java
+++ Java/patterns/patterns.java
@@ -1,105 +1,194 @@
 public class patterns {
     public static void main(String[] args) {
-        // solid pattern
-        /*
-         * int n = 8;
-         * for (int i = 1; i <= n; i++)
-         * 
-         * {
-         * for (int j = 1; j <= n; j++)
-         * 
-         * {
-         * System.out.print("* ");
-         * }
-         * System.out.println();
-         * }
-         */
 
-        // Tringle pattern
-        /*
-         * int n = 5;
-         * for (int i = 1; i<=n; i++){
-         * for(int j = 1; j<=i; j++){
-         * System.out.print("* ");
-         * }
-         * System.out.println();
-         * }
-         */
+        // // solid pattern
 
-        // rombus patterns
-        /*
-         * int n = 8;
-         * for(int i = 1; i<=n; i++){
-         * for(int j = 1; j<=n-i; j++){
-         * System.out.print(" ");
-         * }
-         * for(int j =1; j<=n; j++){
-         * System.out.print("* ");
-         * }
-         * System.out.println();
-         * }
-         */
+        // int n = 8;
+        // for (int i = 1; i <= n; i++)
 
-        // inverted pattern
+        // {
+        // for (int j = 1; j <= n; j++)
 
-        /*
-         * int n =7;
-         * for(int i = n; i>=1; i--){
-         * for(int j = 1; j<=i; j++){
-         * System.out.print("* ");
-         * }
-         * System.out.println();
-         * }
-         */
+        // {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
 
-        // piramid pattern
+        // // Tringle pattern
 
-        /*
-         * int n = 8;
-         * for(int i = 1; i<=n; i++){
-         * for(int j = 1; j<=n-i; j++){
-         * System.out.print(" ");
-         * }
-         * for(int j = 1; j<=2*i-1; j++){
-         * System.out.print("*");
-         * }
-         * System.out.println();
-         * }
-         */
+        // int n = 5;
+        // for (int i = 1; i<=n; i++){
+        // for(int j = 1; j<=i; j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
 
-        // inverted piramid
-        /*
-         * int n = 8;
-         * for (int i = 1; i <= n; i++) {
-         * for (int j=1;j<=i-1; j++) {
-         * System.out.print(" ");
-         * }
-         * for (int j =1; j<=2*(n-i)+1; j++) {
-         * System.out.print("*");
-         * }
-         * System.out.println();
-         * }
-         */
+        // // rombus patterns
 
-        // hollow
+        // int n = 8;
+        // for(int i = 1; i<=n; i++){
+        // for(int j = 1; j<=n-i; j++){
+        // System.out.print(" ");
+        // }
+        // for(int j =1; j<=n; j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
 
-        int n = 4;
-        int m = 6;
+        // // inverted pattern
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        // int n =7;
+        // for(int i = n; i>=1; i--){
+        // for(int j = 1; j<=i; j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
 
-                // sirf boundary pe star print hoga
-                if (i == 1 || i == n || j == 1 || j == m) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
+        // // piramid pattern
 
-            }
-            System.out.println();
-        }
+        // int n = 8;
+        // for(int i = 1; i<=n; i++){
+        // for(int j = 1; j<=n-i; j++){
+        // System.out.print(" ");
+        // }
+        // for(int j = 1; j<=2*i-1; j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
 
+        // // inverted piramid
+
+        // int n = 8;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j=1;j<=i-1; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j =1; j<=2*(n-i)+1; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // // hollow
+
+        // int n = 4;
+        // int m = 6;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= m; j++) {
+
+        // // sirf boundary pe star print hoga
+        // if (i == 1 || i == n || j == 1 || j == m) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print(" ");
+        // }
+
+        // }
+        // System.out.println();
+        // }
+
+        // // hollow right angle tringle
+
+        // int n = 5;
+
+        // for (int i = 1; i<=n; i++) {
+        // for (int j = 1; j<=i; j++) {
+
+        // if (j==1 || j==i || i==n) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print(" ");
+        // }
+
+        // }
+        // System.out.println();
+        // }
+
+        // // hollow piramid
+
+        // int n = 10;
+
+        // for (int i = 1; i<=n; i++) {
+        // for (int j=1; j<=2*n-1; j++) {
+
+        // if (j==n-i+1||j==n+i-1||i==n) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+
+        // }
+        // System.out.println();
+        // }
+
+        // // diamond solid
+
+        // int n = 4;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= 2 * i - 1; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // for (int i = 1; i < n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= 2 * (n - i) - 1; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // // hollow diamond
+
+        // int n = 5;
+
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= 2 * n - 1; j++) {
+
+        //         if (j == n - i + 1 || j == n + i - 1) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+
+        //     }
+        //     System.out.println();
+        // }
+
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = 1; j <= 2 * n - 1; j++) {
+
+        //         if (j == i + 1 || j == 2 * n - i - 1) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+
+
+
+
+        
     }
 
 }
