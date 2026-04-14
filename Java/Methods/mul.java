16429cee5aaf464e5c0a57ab5a9fcee92f586da7
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-04-14 12:34:28 +0530

    Update

---
+++ Java/Methods/mul.java
@@ -0,0 +1,12 @@
+public class mul {
+    static void printMul(int x, int y){
+        int ans = x * y;
+        System.out.println("Mul = " + ans);
+    }    
+    public static void main() {
+        printMul(6,8);
+    }
+
+}
+
+
---
+++ Java/Methods/nonvoid.java
@@ -0,0 +1,13 @@
+public class nonvoid {
+
+    static int add(int x, int y){
+        int mul = x*y;
+        return mul;
+    }
+
+    public static void main(){
+        int result = add(8,9);
+        System.out.println("Result " + result);
+    }
+    
+}
